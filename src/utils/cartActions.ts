39fb81d4b034169cf3ecd2c3d602e18af6912bbd
Іван Вигінний <ivan665303@gmail.com>
2023-09-11 21:12:04 +0300

    Fixed undefined trouble 2

--- src/utils/cartActions.ts
+++ src/utils/cartActions.ts
@@ -4,13 +4,13 @@ import { Product } from "@/types/Product";
 export const addToCart = (product: Product, cart: CartItem[], setCart: any) => {
   if (product) {
     const existingCartItem = cart.find(
-      cartItem => cartItem.item._id === product._id
+      cartItem => cartItem?.item?._id === product._id
     );
 
     if (existingCartItem) {
       setCart(
         cart.map(cartItem =>
-          cartItem.item._id === product._id
+          cartItem?.item?._id === product._id
             ? { ...cartItem, qty: cartItem.qty + 1 }
             : cartItem
         )
