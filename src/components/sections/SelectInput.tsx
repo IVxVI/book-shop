5a8cb20eb3e3b127748894257beeaace9b02b93a
Іван Вигінний <ivan665303@gmail.com>
2023-10-02 21:48:52 +0300

    Added new product features, search and changer orders list

--- src/components/sections/SelectInput.tsx
+++ src/components/sections/SelectInput.tsx
@@ -35,7 +35,7 @@ export const SelectInput: FC<Props> = ({options}) => {
               <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {options.map((option) => (
                   <Listbox.Option
-                    key={option}
+                    key={options.indexOf(option)}
                     className={({ active }) =>
                       classNames(
                         active ? 'bg-indigo-600 text-white' : 'text-gray-900',
